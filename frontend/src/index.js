623d468acd6a4f8f575b61474bd95a9f10a989cc
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-09-27 08:43:07 -0700

    custom bootstrap styles

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./index.css";
+import "./assets/styles/bootstrap.custom.css"; // Importing Bootstrap CSS file
+import "./assets/styles/index.css"; // Importing CSS file
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
