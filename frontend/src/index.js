cfd181cdee87f9bfad9adced3f86fa63edae0915
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-09-29 10:10:21 -0700

    router links for nav and products

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,17 +6,22 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
 import "./assets/styles/bootstrap.custom.css"; // Importing Bootstrap CSS file
 import "./assets/styles/index.css"; // Importing CSS file
 import App from "./App";
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route path='/' element={<App />}></Route>)
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route index={true} path='/' element={<HomeScreen />} />
+    </Route>
+  )
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
