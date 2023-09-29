7e2716080b59b38fcec4dd3ec8babcbb0c06849b
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-09-29 09:59:54 -0700

    router in index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import "./assets/styles/bootstrap.custom.css"; // Importing Bootstrap CSS file
 import "./assets/styles/index.css"; // Importing CSS file
 import App from "./App";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path='/' element={<App />}></Route>)
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
