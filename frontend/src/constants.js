4a7f6c38b89546959dee24443cf7b20ed9073a58
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-10-01 10:22:57 -0700

    url constants

---
+++ frontend/src/constants.js
@@ -0,0 +1,10 @@
+export const BASE_URL =
+  process.env.NODE_ENV === "development" ? "http://localhost:5000" : "";
+
+export const PRODUCTS_URL = `${BASE_URL}/api/products`;
+
+export const PAYPAL_URL = `${BASE_URL}/api/config/paypal`;
+
+export const ORDERS_URL = `${BASE_URL}/api/orders`;
+
+export const USERS_URL = `${BASE_URL}/api/users`;
