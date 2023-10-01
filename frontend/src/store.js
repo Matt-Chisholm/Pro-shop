c9697a13f19f8264dc9325d52772908fafc70364
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-10-01 10:29:27 -0700

    api slice

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
-import rootReducer from "./reducers/exampleReducer";
+import { apiSlice } from "./slices/apiSlice";
 
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: true,
 });
 
 export default store;
