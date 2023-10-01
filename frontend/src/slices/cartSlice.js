787cb7e66013b3fa93c9aad321807768cb115c09
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-10-01 14:41:32 -0700

    cart slice reducer

---
+++ frontend/src/slices/cartSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = localStorage.getItem("cart")
+  ? JSON.parse(localStorage.getItem("cart"))
+  : { cartItems: [], shippingAddress: {}, paymentMethod: "" };
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {},
+});
+
+export const {
+  addToCart,
+  removeFromCart,
+  saveShippingAddress,
+  savePaymentMethod,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
