b4bbb3db848199d9a89ec45d8abe225f0c68d822
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-10-02 15:59:21 -0700

    add to cart reducer

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -4,10 +4,39 @@ const initialState = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart"))
   : { cartItems: [], shippingAddress: {}, paymentMethod: "" };
 
+const addDemicals = (num) => {
+  return (Math.round(num * 100) / 100).toFixed(2);
+};
+
 const cartSlice = createSlice({
   name: "cart",
   initialState,
-  reducers: {},
+  reducers: {
+    addToCart: (state, action) => {
+      const item = action.payload;
+
+      const existItem = state.cartItems.find((x) => x._id === item._id);
+
+      if (existItem) {
+        state.cartItems = state.cartItems.map((x) =>
+          x._id === existItem._id ? item : x
+        );
+      } else {
+        state.cartItems = [...state.cartItems, item];
+      }
+
+      // Calculate item's price
+      state.itemsPrice = state.cartItems.reduce(
+        (acc, item) => acc + item.price * item.qty,
+        0
+      );
+      //Calculate shipping price
+
+      // Calculate tax price
+
+      // Calculate total price
+    },
+  },
 });
 
 export const {
