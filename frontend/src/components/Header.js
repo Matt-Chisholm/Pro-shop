b245a3c42e4a4380a8b33083c75f8ff1058290be
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-10-07 09:47:25 -0700

    cart qty showing on header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { LinkContainer } from "react-router-bootstrap";
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Container, Badge, Nav, Navbar } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
+import { useSelector } from "react-redux";
 import logo from "../assets/logo.png";
 
 const Header = () => {
+  const { cartItems } = useSelector((state) => state.cart);
+
   return (
     <header>
       <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
@@ -20,6 +23,11 @@ const Header = () => {
           <Nav className='ms-auto'>
             <LinkContainer to='/cart'>
               <Nav.Link>
+                {cartItems.length > 0 && (
+                  <Badge bg='success' pill className='me-2'>
+                    {cartItems.reduce((acc, item) => acc + item.qty, 0)}
+                  </Badge>
+                )}
                 <FaShoppingCart /> Cart
               </Nav.Link>
             </LinkContainer>
