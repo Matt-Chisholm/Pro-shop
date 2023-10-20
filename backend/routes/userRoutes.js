9e632cc76751485c727ad9a719943c11d36d7c7f
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-10-20 10:10:36 -0700

    userRoutes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -12,3 +12,11 @@ import {
 import express from "express";
 
 const router = express.Router();
+
+router.route("/").post(registerUser).get(getUsers);
+router.post("/logout", logoutUser);
+router.post("/login", authUser);
+router.route("/profile").get(getUserProfile).put(updateUserProfile);
+router.route("/:id").delete(deleteUser).get(getUserById).put(updateUser);
+
+export default router;
