0bc5042bde4b95755dcef9852c680c61e9ad0f5f
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-10-20 10:14:17 -0700

    body parser middleware

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,9 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
+// Body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
