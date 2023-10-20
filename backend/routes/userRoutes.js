d7489d8e3da7b87fe6a6a60a7487cbf3667af113
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-10-20 10:05:16 -0700

    userroutes imports

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,14 @@
+import {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+  getUserById,
+  updateUser,
+} from "../controllers/userController";
+import express from "express";
+
+const router = express.Router();
