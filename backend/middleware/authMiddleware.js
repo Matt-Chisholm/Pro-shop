134364437e71aa2071815d69e6ed872f19868dd8
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-10-25 14:17:09 -0700

    auth middleware funcs

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,45 @@
+import jwt from "jsonwebtoken";
+import asyncHandler from "./asyncHandler.js";
+import User from "../models/userModel.js";
+
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  // Check if authorization header exists and starts with "Bearer"
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      // Get token from authorization header
+      token = req.headers.authorization.split(" ")[1];
+
+      // Verify token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      // Find user by id from decoded token
+      req.user = await User.findById(decoded.userId).select("-password");
+
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(401);
+      throw new Error("Not authorized, token failed");
+    }
+  } else {
+    // No authorization header
+    res.status(401);
+    throw new Error("Not authorized, no token");
+  }
+});
+
+const admin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401);
+    throw new Error("Not authorized as an admin");
+  }
+};
+
+export { protect, admin };
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
@@ -11,10 +12,12 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
-// Body parser
+
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Server is ready");
