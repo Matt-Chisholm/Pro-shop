90cb3147fe371e9ec872d69890979e63820f633c
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-10-28 16:58:19 -0700

    hash password in model before saving user

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -18,6 +18,20 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
+// Encrypt password before saving user
+userSchema.pre("save", async function (next) {
+  // if password is not modified, call next middleware
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  // generate salt
+  const salt = await bcrypt.genSalt(10);
+
+  // hash password
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
 const User = mongoose.model("User", userSchema);
 
 export default User;
