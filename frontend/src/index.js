52e0e99fcda4b8654952cc2eb33789ae118a5a69
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-10-29 13:28:42 -0700

    initial register screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,6 +10,7 @@ import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
 import "./assets/styles/bootstrap.custom.css"; // Importing Bootstrap CSS file
 import "./assets/styles/index.css"; // Importing CSS file
 import App from "./App";
@@ -23,6 +24,7 @@ const router = createBrowserRouter(
       <Route path='/product/:id' element={<ProductScreen />} />
       <Route path='/cart' element={<CartScreen />} />
       <Route path='/login' element={<LoginScreen />} />
+      <Route path='/register' element={<RegisterScreen />} />
     </Route>
   )
 );
---
+++ frontend/src/screens/RegisterScreen.js
@@ -0,0 +1,98 @@
+import React, { useState, useEffect } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Form, Button, Row, Col } from "react-bootstrap";
+import FormContainer from "../components/FormContainer";
+import { useDispatch, useSelector } from "react-redux";
+import Loader from "../components/Loader";
+import { useRegisterMutation } from "../slices/usersApiSlice";
+import { setCredentials } from "../slices/authSlice";
+import { toast } from "react-toastify";
+
+function RegisterScreen() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [name, setName] = useState("");
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [register, { isLoading }] = useRegisterMutation();
+
+  // This will be used to check if the user is already logged in.
+  const { userInfo } = useSelector((state) => state.auth);
+
+  // These functions are used to redirect the user to the page they were on before they logged in.
+  const { search } = useLocation();
+  const searchParams = new URLSearchParams(search);
+  const redirect = searchParams.get("redirect") || "/";
+
+  // If the user is already logged in, redirect them to the home page.
+  useEffect(() => {
+    if (userInfo) {
+      navigate(redirect);
+    }
+  }, [navigate, redirect, userInfo]);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      toast.error("Passwords do not match");
+      return;
+    }
+    // DISPATCH LOGIN
+    try {
+      // This is the new login mutation that we created in frontend/src/slices/usersApiSlice.js.
+      const result = await register({ email, password, name }).unwrap();
+      dispatch(setCredentials({ ...result }));
+      navigate(redirect);
+    } catch (error) {
+      console.log(error);
+      toast.error(error?.data?.message || error.message);
+    }
+  };
+
+  return (
+    <FormContainer>
+      <h1>Sign Up</h1>
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId='email' className='my-3'>
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type='email'
+            placeholder='Enter email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId='password' className='my-3'>
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type='password'
+            placeholder='Enter password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}></Form.Control>
+        </Form.Group>
+
+        <Button
+          type='submit'
+          variant='primary'
+          className='mt-2'
+          disabled={isLoading}>
+          Sign In
+        </Button>
+        {isLoading && <Loader />}
+      </Form>
+
+      <Row className='py-3'>
+        <Col>
+          New Customer?{" "}
+          <Link to={redirect ? `/register?redirect=${redirect}` : "/register"}>
+            Register
+          </Link>
+        </Col>
+      </Row>
+    </FormContainer>
+  );
+}
+
+export default RegisterScreen;
