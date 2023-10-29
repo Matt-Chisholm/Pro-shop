49cbad707d3a4da6d0329ea96c58033f02ac7d46
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-10-29 11:37:51 -0700

    reuseable form container component

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -1,6 +1,7 @@
 import { USERS_URL } from "../constants";
 import { apiSlice } from "./apiSlice";
 
+// This is a new slice that will be used to make API calls to the users endpoint.
 export const usersApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     login: builder.mutation({
