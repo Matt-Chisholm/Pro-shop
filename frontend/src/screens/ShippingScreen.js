acbcd8cca1afb627c7ee70837046055e58d71b87
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-11-03 12:12:46 -0700

    address consistent with state

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -1,21 +1,34 @@
 import React from "react";
 import { useState } from "react";
 import { Form, Button } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { saveShippingAddress } from "../slices/cartSlice";
 import FormContainer from "../components/FormContainer";
 
 function ShippingScreen() {
-  const [address, setAddress] = useState("");
-  const [city, setCity] = useState("");
-  const [postalCode, setPostalCode] = useState("");
-  const [country, setCountry] = useState("");
+  const cart = useSelector((state) => state.cart);
+  const { shippingAddress } = cart;
 
-  function submitHandler(e) {
+  const [address, setAddress] = useState(shippingAddress?.address || "");
+  const [city, setCity] = useState(shippingAddress?.city || "");
+  const [postalCode, setPostalCode] = useState(
+    shippingAddress?.postalCode || ""
+  );
+  const [country, setCountry] = useState(shippingAddress?.country || "");
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const submitHandler = (e) => {
     e.preventDefault();
-    console.log("submit");
-  }
+    dispatch(saveShippingAddress({ address, city, postalCode, country }));
+    navigate("/payment");
+  };
 
   return (
     <FormContainer>
+      <h1>Shipping</h1>
       <Form onSubmit={submitHandler}>
         <Form.Group controlId='address'>
           <Form.Label>Address</Form.Label>
@@ -57,7 +70,7 @@ function ShippingScreen() {
             onChange={(e) => setCountry(e.target.value)}></Form.Control>
         </Form.Group>
 
-        <Button type='submit' variant='primary'>
+        <Button type='submit' variant='primary' className='my-2'>
           Continue
         </Button>
       </Form>
