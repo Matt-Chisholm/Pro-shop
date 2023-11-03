4821386ec058f30338d7b24356709adf8994a9b1
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-11-03 12:34:12 -0700

    payment screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,6 +12,7 @@ import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import ShippingScreen from "./screens/ShippingScreen";
+import PaymentScreen from "./screens/PaymentScreen";
 import PrivateRoute from "./components/PrivateRoute";
 import "./assets/styles/bootstrap.custom.css";
 import "./assets/styles/index.css"; // Importing CSS file
@@ -29,6 +30,7 @@ const router = createBrowserRouter(
       <Route path='/register' element={<RegisterScreen />} />
       <Route path='' element={<PrivateRoute />}>
         <Route path='/shipping' element={<ShippingScreen />} />
+        <Route path='/payment' element={<PaymentScreen />} />
       </Route>
     </Route>
   )
