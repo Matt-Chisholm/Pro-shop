31092fd99c6c2a47f3bda45e2a062147679a53c7
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-11-11 09:50:38 -0800

    delete route

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,6 +3,7 @@ import {
   getProducts,
   updateProduct,
   createProduct,
+  deleteProduct,
   getProductById,
 } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
@@ -10,6 +11,10 @@ import { protect, admin } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 router.route("/").get(getProducts).post(protect, admin, createProduct);
-router.route("/:id").get(getProductById).put(protect, admin, updateProduct);
+router
+  .route("/:id")
+  .get(getProductById)
+  .put(protect, admin, updateProduct)
+  .delete(protect, admin, deleteProduct);
 
 export default router;
