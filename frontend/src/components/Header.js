ecb87dc03756d3aea396b61d0f234517b70de370
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-11-13 12:58:57 -0800

    search bar working

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -6,6 +6,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { useLogoutMutation } from "../slices/usersApiSlice";
 import { logout } from "../slices/authSlice";
 import { useNavigate } from "react-router-dom";
+import SearchBar from "./SearchBar";
 import logo from "../assets/logo.png";
 
 const Header = () => {
@@ -40,6 +41,7 @@ const Header = () => {
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'></Navbar.Collapse>
           <Nav className='ms-auto'>
+            <SearchBar />
             <LinkContainer to='/cart'>
               <Nav.Link>
                 {cartItems.length > 0 && (
--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -24,9 +24,10 @@ const SearchBar = () => {
         type='text'
         name='q'
         onChange={(e) => setKeyword(e.target.value)}
+        value={keyword}
         placeholder='Search Products...'
         className='mr-sm-2 ml-sm-5'></Form.Control>
-      <Button type='submit' variant='outline-success' className='p-2'>
+      <Button type='submit' variant='outline-light' className='p-2 mx-2'>
         Search
       </Button>
     </Form>
