5ab01a5b62908650280ae3912df4ec1b713d4e1e
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-11-13 11:38:37 -0800

    create review controller function

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -97,10 +97,60 @@ const deleteProduct = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc   Create new review
+// @route  POST /api/products/:id/reviews
+// @access Private
+const createProductReview = asyncHandler(async (req, res) => {
+  const { rating, comment } = req.body;
+
+  // Get the product
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    const alreadyReviewed = product.reviews.find(
+      (review) => review.user.toString() === req.user._id.toString()
+    );
+
+    if (alreadyReviewed) {
+      res.status(400);
+      throw new Error("Product already reviewed");
+    }
+
+    // Create the review
+    const review = {
+      name: req.user.name,
+      rating: Number(rating),
+      comment,
+      user: req.user._id,
+    };
+
+    // Add the review to the product
+    product.reviews.push(review);
+
+    // Update the number of reviews
+    product.numReviews = product.reviews.length;
+
+    // Update the rating
+    product.rating =
+      product.reviews.reduce((acc, review) => review.rating + acc, 0) /
+      product.reviews.length;
+
+    // Save the product
+    await product.save();
+
+    // Send a response
+    res.status(201).json({ message: "Review added" });
+  } else {
+    res.status(404);
+    throw new Error("Resource not found");
+  }
+});
+
 export {
   getProducts,
   getProductById,
   createProduct,
   updateProduct,
   deleteProduct,
+  createProductReview,
 };
