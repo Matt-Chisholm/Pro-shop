023c4edbdef5b5c5cc32e5fd3ccd29575f191db8
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-11-13 14:01:17 -0800

    meta title for product pages

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -7,6 +7,7 @@ import {
   useCreateReviewMutation,
 } from "../slices/productsApiSlice";
 import Loader from "../components/Loader";
+import Meta from "../components/Meta";
 import Message from "../components/Message";
 import { addToCart } from "../slices/cartSlice";
 import { useDispatch, useSelector } from "react-redux";
@@ -65,6 +66,7 @@ const ProductScreen = () => {
         </Message>
       ) : (
         <>
+          <Meta title={product.name} />
           <Link className='btn btn-light my-3' to='/'>
             Go Back
           </Link>
